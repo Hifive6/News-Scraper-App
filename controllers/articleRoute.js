15c1beb77f37751cb58b9f3ae5429cac20c742fb
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-11-04 00:10:51 -0400

    scraped info

--- controllers/articleRoute.js
+++ controllers/articleRoute.js
@@ -2,4 +2,4 @@
 
 
 var Comment = require("../models/Comment.js");
-var Article = require("../models/Article.js");
+var Article = require("../models/Article.js");""
--- server.js
+++ server.js
@@ -11,21 +11,43 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 
-var db = require("./models");
+var db = require("./models/");
 
 var PORT = 3000;
 
 var app = express();
 
 app.use(express.static("public"));
-app.use("/", htmlRouter);
-app.use("/", articleRouter)
+//app.use("/", htmlRouter);
+//app.use("/", articleRouter)
 
 mongoose.connect("mongodb://localhost/newScraper");
 
+app.get("/scraped", function(req, res){
+    axios.get("https://www.npr.org/sections/news/archive")
+    .then(function(response){
+        var $ = cheerio.load(response.data);
 
-
-
+        $("div.archivelist > article").each(function(i, element){
+            
+            var result ={};
+            
+            //result.title = $(this).children().find("a").text()
+            result.title = $(element).children().find("h2.title").text()
+            result.summary = $(element).children().find("p.teaser").text()
+            result.link = $(element).children().find("a").attr("href")
+            
+            db.Article.create(result)
+            .then(function(dbArticle){
+                console.log(dbArticle);
+            })
+            .catch(function(err){
+                return res.json(err);
+            })
+        })
+        res.send("Scraped Complete")
+    })
+})
 
 
 
